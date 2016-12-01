0fc21db213093302eec72ee86fe11c72958e445d
Johann Mortara <johann.mortara@etu.unice.fr>
2016-12-02 00:46:58 +0100

    Récupération des tweets

--- twitter.php
+++ twitter.php
@@ -15,7 +15,17 @@ require_once('twitteroauth/autoload.php');
 $connection = new TwitterOAuth($consumer_key, $consumer_secret, $oauth_token, $oauth_token_secret);
 
 //4 - Start Querying
-$content = $connection->get("search/tweets", ["q" => "%23nuitinfo", "count" => 100]); // récupère les 100 derniers tweets avec le hashtag #nuitinfo
-var_dump($content);
+$content = $connection->get("search/tweets", ["q" => "%23nuitinfo", "count" => 30]); // récupère les 100 derniers tweets avec le hashtag #nuitinfo
+//var_dump($content);
+$array = $array = json_decode(json_encode($content), true);
+$arrayResult = array();
+for($i=0 ; $i < 30 ; $i++){
+    array_push($arrayResult, array("text" => $array["statuses"][$i]["text"],
+        "name" => $array["statuses"][$i]["user"]["name"],
+        "geo" => $array["statuses"][$i]["geo"]));
+}
+var_dump($arrayResult);
+$arrayJson = json_encode($arrayResult);
+echo $arrayJson;
 //$tweet = $connection->post("statuses/update", ["status" => "L'API Twitter marche enfin ! #nuitinfo"]); // tweet un message
 ?>
