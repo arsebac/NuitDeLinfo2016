f6b7bc1990cf57e7e04d7eb376c4671367d6bc33
Johann Mortara <johann.mortara@etu.unice.fr>
2016-12-02 03:53:52 +0100

    Envoi_location

--- Envoi_location.php
+++ Envoi_location.php
@@ -7,11 +7,11 @@
  */
 // Create connection
 require_once "bdConnection.php";
-$sql = "INSERT INTO `location` (`idLocation`, `lat`, `lng`, `name`, `desc`)
+$sql = "INSERT INTO `location` (`idLocation`, `lat`, `lng`, `name`, `type`)
 VALUES (NULL, '".filter_input(INPUT_GET, 'latitude', FILTER_SANITIZE_ENCODED)."',
 '".filter_input(INPUT_GET, 'longitude', FILTER_SANITIZE_ENCODED)."',
 '".filter_input(INPUT_GET, 'name', FILTER_SANITIZE_ENCODED)."',
-'".filter_input(INPUT_GET, 'description', FILTER_SANITIZE_ENCODED)."')";
+'".filter_input(INPUT_GET, 'type', FILTER_SANITIZE_ENCODED)."')";
 echo $sql;
 $res = $mysqli->query($sql);
 ?>
