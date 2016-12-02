41c2d55e9eb178f8da53556b9b8814c517956d62
Francois Melkonian <francois.melkonian@etu.unice.fr>
2016-12-02 05:44:06 +0100

    Modif connection

--- getLocation.php
+++ getLocation.php
@@ -7,15 +7,16 @@
  */
 
 require_once "bdConnection.php";
-echo "yolo";
     $arrayResult = array();
     $res = $mysqli->query("SELECT `lat`,`lng`,`name`,`type` FROM `location`");
 
-
+echo '{"pts":[';
 for ($row_no = 0; $row_no < $res->num_rows; $row_no++) {
     $res->data_seek($row_no);
     $row = $res->fetch_assoc();
-    array_push($arrayResult, array("lat"=>$row["lat"],"lng"=>$row["lng"],"name"=>$row["name"],"type"=>$row["type"]));
+    echo json_encode(array("lat"=>$row["lat"],"lng"=>$row["lng"],"name"=>$row["name"],"type"=>$row["type"]));
+    if($row_no - 1  != $res->num_rows){
+        echo ",";
+    }
 }
-$finalResult = array("pts" => $arrayResult);
-echo json_encode($finalResult);
+echo "]}";
